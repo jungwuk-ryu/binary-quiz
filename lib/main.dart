36dfb25c035c9135eba9777a2bc9707033bc7c66
jungwuk <vojougae35@gmail.com>
2024-08-14 22:13:33 +0900

    :sparkles: feat: 앱 시작 전면광고 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
 import 'dart:ui';
 
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:binary_quiz/ui/themes/app_colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
@@ -13,6 +17,7 @@ import 'routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
 
   await Future.wait([
     Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
@@ -29,7 +34,47 @@ void main() async {
     return true;
   };
 
+  // 추적 허용 다이얼로그
+  if (await AppTrackingTransparency.trackingAuthorizationStatus ==
+      TrackingStatus.notDetermined) {
+    await AppTrackingTransparency.requestTrackingAuthorization();
+  }
+
+  _showInterstitialAd();
+
   runApp(const MyApp());
+}
+
+Future<void> _showInterstitialAd() async {
+  InterstitialAd? interstitialAd;
+
+  late String adUnitId;
+  if (kDebugMode) {
+    adUnitId = Platform.isAndroid
+        ? 'ca-app-pub-3940256099942544/1033173712'
+        : 'ca-app-pub-3940256099942544/4411468910';
+  } else {
+    adUnitId = Platform.isAndroid
+        ? throw UnimplementedError()
+        : 'ca-app-pub-3321763933554531/4969851808';
+  }
+
+  await InterstitialAd.load(
+      adUnitId: adUnitId,
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        // Called when an ad is successfully received.
+        onAdLoaded: (ad) {
+          debugPrint('$ad loaded.');
+          // Keep a reference to the ad so you can show it later.
+          interstitialAd = ad;
+          interstitialAd?.show();
+        },
+        // Called when an ad request failed.
+        onAdFailedToLoad: (LoadAdError error) {
+          debugPrint('InterstitialAd failed to load: $error');
+        },
+      ));
 }
 
 class MyApp extends StatelessWidget {
